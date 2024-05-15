0e4870724587ea36467b79eef7296e2f140c9c59
Marcelinc <marcin.c40@wp.pl>
2024-05-15 14:23:27 +0200

    Templates of the Landing and Register pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register', function (Request $request) {
+    return view('register',[
+        'type' => $request->type
+    ]);
 });
