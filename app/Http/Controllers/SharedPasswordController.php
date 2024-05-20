7b1ece9340eae43bdd796a7bd3e4392831edd1a8
Marcelinc <marcin.c40@wp.pl>
2024-05-20 11:15:46 +0200

    SharedPassword controller with fetching data

--- app/Http/Controllers/SharedPasswordController.php
+++ app/Http/Controllers/SharedPasswordController.php
@@ -16,34 +16,19 @@ class SharedPasswordController extends Controller
             'owner_login' => User::select('name')->whereColumn('id','shared_passwords.id_owner'),
             'password' => Password::select('password','login')->whereColumn('id','shared_passwords.id_password')])
         ->where('id_receiver',2)->get();*/
-        //$sharedPasswords = SharedPassword::join('users','users.id', '=','shared_password.id_owner')
-       // ->get();
+        $sharedPasswords = SharedPassword::join('users','users.id', '=','shared_passwords.id_owner')
+        ->join('passwords','passwords.id', '=','shared_passwords.id_password')
+        ->join('websites','websites.id', '=','passwords.website_id')
+        ->where('id_receiver',2)->get(['shared_passwords.valid','shared_passwords.id_owner','users.name','shared_passwords.id_password','passwords.password','passwords.login','websites.name as website']);
 
-       // dd($sharedPasswords);
+        //dd($sharedPasswords);
 
         return view('dashboard',[
             "content" => "sharedPasswords",
             "login" => "User1",
             "isHmac" => true,
             'passwordCount' => 2,
-            "passwords" => [
-                [
-                    "id" => 1,
-                    "valid" => true,
-                    "owner" => [
-                        "id" => 1,
-                        "login" => "User1"
-                    ],
-                    "password" => [
-                        "id" => 3,
-                        "value" => "password",
-                        "login" => "User12",
-                        "website" => [
-                            "name" => "Youtube",
-                        ]
-                    ]
-                ]
-            ]
+            "passwords" => $sharedPasswords
         ]);
     }
 }
