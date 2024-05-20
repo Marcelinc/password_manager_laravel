3c8d839dbf2c3cab1a3ebc3df14a29e19c5990f3
Marcelinc <marcin.c40@wp.pl>
2024-05-20 22:56:31 +0200

    User registration

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,16 @@
-@php
-    $auth = false;
-@endphp
 <nav>
-    @if($auth)
-        <a href="/logout" id="logout">Logout</a>
+    @auth
+        <div id="logout">
+            <form method="POST" action="/logout">
+                @csrf
+                <button type="submit" id="logout-button">Logout</button>
+            </form>
+        </div>
     @else
         <a href="/">PasswordWallet</a>
         <div class="authLinks">
             <a href="/login">Log in</a>
             <a href="/register">Register</a>
         </div>
-    @endif
+    @endauth
 </nav>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,25 +4,36 @@
     <main class="formPage">
         <div class="form-container">
             <h1>Create an account</h1>
-            <form class="form">
+            <form method="POST" action="/create-user" class="form">
+                @csrf
                 <label class="formElem">
                     <p>Login</p>
-                    <input type='text' name="login"/>
+                    <input type='text' name="username" value="{{old('username')}}"/>
+                    
                 </label>
+                @error('username')
+                    <p class="appMessage">{{$message}}</p>
+                @enderror
                 <label class="formElem">
                     <p>Password</p>
-                    <input type='password' name="password"/>    
+                    <input type='password' name="password" value="{{old('password')}}"/>
                 </label>
+                @error('password')
+                    <p class="appMessage">{{$message}}</p>
+                @enderror
                 <div class="formRadio">
                     <label>
-                        <input type="radio" name="accountType" value='sha512' id="sha512" <?=$type=='sha512' ? "checked" : ""?>/>
+                        <input type="radio" name="accountType" value='sha512' id="sha512" <?=($type=='sha512' ? "checked" : old('accountType')=="sha512") ? "checked" : ""?>/>
                         SHA512
                     </label>
                     <label>
-                        <input type="radio" name="accountType" value='hmac' id="hmac" <?=$type=='hmac' ? "checked" : ""?>/>
+                        <input type="radio" name="accountType" value='hmac' id="hmac" <?=($type=='hmac' ? "checked" : old('accountType')=="hmac") ? "checked" : ""?>/>
                         HMAC
                     </label>
                 </div>
+                @error('accountType')
+                        <p class="appMessage">{{$message}}</p>
+                @enderror
                 <div class="formElem">
                     <input type="submit" class="submit" value="Register"/>
                 </div>
