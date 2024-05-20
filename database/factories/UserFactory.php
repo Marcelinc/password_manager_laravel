66a9f748d7eff8c29c2d13fadd33139b320e5d7e
Marcelinc <marcin.c40@wp.pl>
2024-05-20 11:50:28 +0200

    IpAddress, LoginAttempt and SharedPassword seeders, changed name column to username in users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->unique()->name(),
+            'username' => fake()->unique()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
